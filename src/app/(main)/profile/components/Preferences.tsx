1fc07ccdf7267b93675082afd4936ef5eeb7e00a
Lorant <lorant@once-ui.com>
2025-04-15 21:32:52 +0200

    add stylepanel to preferences

--- src/app/(main)/profile/components/Preferences.tsx
+++ src/app/(main)/profile/components/Preferences.tsx
@@ -6,13 +6,15 @@ import {
   Heading,
   Text,
   Row,
-  Button
+  Button,
+  StylePanel
 } from "@/once-ui/components";
 
 export default function Preferences() {
   return (
     <Column gap="24" fill>
       <Column gap="16" padding="24" fill>
+      <StylePanel/>
       </Column>
     </Column>
   );
